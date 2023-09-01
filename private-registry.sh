0ac5f8b1430c4a0b9c73720b7773e5541f88d1ad
Robert Gabriel <ephemeric@icloud.net>
2023-09-01 11:38:28 +0000

    Notes.

--- private-registry.sh
+++ private-registry.sh
@@ -2,14 +2,14 @@
 
 docker run -d \
   --restart=always \
-  --name privreg \
+  --name private-registry \
   -v $PWD/i.crt:/i.crt \
   -v $PWD/i.key:/i.key \
-  -v /media/data1/docker/privreg:/var/lib/registry
+  -v /media/data1/docker:/var/lib/registry/docker
   -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/i.crt \
   -e REGISTRY_HTTP_TLS_KEY=/i.key \
-  -p 192.168.122.2:443:443 \
+  -p 192.168.122.3:443:443 \
   registry:2
 
 exit 0
--- ptp-registry-1.docker.io.sh
+++ ptp-registry-1.docker.io.sh
@@ -2,16 +2,15 @@
 
 docker run -d \
 --restart=always \
---name registry-registry.k8s.io \
+--name ptp-registry-1.docker.io \
 -v $PWD/i.crt:/i.crt \
 -v $PWD/i.key:/i.key \
--v /media/data1/docker/proxy:/var/lib/registry \
+-v /media/data1/docker:/var/lib/registry/docker \
 -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
 -e REGISTRY_HTTP_TLS_CERTIFICATE=/i.crt \
 -e REGISTRY_HTTP_TLS_KEY=/i.key \
 -e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io \
 -p 192.168.122.2:443:443 \
--p 127.0.0.1:443:443 \
 registry:2
 
 exit 0
